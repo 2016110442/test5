fa3c0c8d252efc6e5fc117c96d8e910d8c3ba0dd
Wu <wu@wudemacbook-air.local>
2018-12-17 23:23:25 +0800

    Initial Commit

---
+++ 2016110442/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2016110442
+//
+//  Created by Wu on 2018/12/17.
+//  Copyright © 2018年 wyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
